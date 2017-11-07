cc11e6af407070ec65e38b3ead144a0853b19cfe
ihfisk <ihfisk42@gmail.com>
2017-11-07 16:16:21 +0100

    ajout de prototypes

--- ensemble.h
+++ ensemble.h
@@ -1,4 +1,9 @@
 #define TAILLE 1000
 typedef int ensemble[TAILLE]
 
-  
+// ens_creerEnsemble permet de creer un ensemble a partir d'un tableau d'entier, dont les entiers correspondent au éléments de l'ensemble.  
+ensemble ens_creerEnsemble(int tableau[],int tailleTableau);
+
+void ens_afficher(ensemble e);
+
+void ens_ajouterEnsemble(ensemble e, int element);
