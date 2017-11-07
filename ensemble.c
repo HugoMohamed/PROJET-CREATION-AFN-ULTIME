9a0f48efb07b09d7273659e1d5e6049a1f37abb1
IhFisk <ihfisk42@gmail.com>
2017-11-07 16:04:00 +0100

    new world

--- ensemble.c
+++ ensemble.c
@@ -1,34 +1,36 @@
 #include "ensemble.h"
 
-void ens_ensemble(int tableau[],int tailleTableau)
+ensemble ens_creerEnsemble(int tableau[],int tailleTableau)
 {
-  int i, int taille = 0;
-  ensemble e;
-  e[0]=0;
+    int i;
+    ensemble e;
+    e[0]=0;
   
-  for(i=1;i<tailleTableau;i++)
-  {
-    if(tableau[i] == i)
-      {
-	e[i] = 1;
-	e[0]++;
-      }
-    else
-      {
-	e[i]=0;
-      }
-  }
+    for(i=1;i<tailleTableau;i++)
+    {
+	if(tableau[i] == i)
+	{
+	    e[i] = 1;
+	    e[0]++;
+	}
+	else
+	{
+	    e[i]=0;
+	}
+    }
+    return e;
 }
+
 
 void ens_afficher(ensemble e)
 {
-  int i;
+    int i;
 
-  for(i=1;i<e[0];i++)
-  {
-    if(e[i] == 1)
+    for(i=1;i<e[0];i++)
     {
-      printf("%d ",i);
+	if(e[i] == 1)
+	{
+	    printf("%d ",i);
+	}
     }
-  }
 }
