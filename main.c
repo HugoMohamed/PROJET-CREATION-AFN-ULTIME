786a6723d71b8934298d1d0a9fb27fde45929773
Sicilien <32875283+Sicilien@users.noreply.github.com>
2017-11-08 20:46:10 +0100

    main.c

--- main.c
+++ main.c
@@ -2,11 +2,10 @@
 
 int main(int argc, char *argv[])
 {
-    int tab[TAILLE]={1,2,3};
-    int tabA[TAILLE] = {4,5,6,7,8,9};
-    int tabB[TAILLE] = {7,8,9,10,11,12};
-    int i;
-    
+    int tab[TAILLE]={5,6,8};
+    int tabA[TAILLE] = {4,5,6,89};
+    int tabB[TAILLE] = {6,7,8,9,12,89};
+
     ensemble e,a,b,uni,inter;
     
     ens_creerEnsemble(tab,4,e);
@@ -15,9 +14,11 @@ int main(int argc, char *argv[])
     ens_ajouterElement(e,4);
     ens_afficher(e);
 
-    ens_creerEnsemble(tabA,6,a);
-    ens_creerEnsemble(tabB,6,b);
-    
+    ens_creerEnsemble(tabA,4,a);
+    ens_creerEnsemble(tabB,5,b);
+
+    ens_afficher(a);
+    ens_afficher(b);
     ens_union(a,b,uni);
     ens_afficher(uni);
 
