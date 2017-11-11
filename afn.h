5b37693ae53d90f262d0e972c433e2c7852ac5d3
ihfisk <ihfisk42@gmail.com>
2017-11-11 16:40:40 +0100

    ajout de prototypes

--- afn.h
+++ afn.h
@@ -1,10 +1,10 @@
 /******************************************
-*
-* Nom du fichier : afn.h
-*
-* Description: header de afn.c
-*
-******************************************/
+ *
+ * Nom du fichier : afn.h
+ *
+ * Description: header de afn.c
+ *
+ ******************************************/
 
 
 #include "ensemble.h"
@@ -14,10 +14,13 @@ typedef char* alphabet[TAILLE];
 
 typedef struct
 {
-  ensemble transition[TAILLE][TAILLE];
-  ensemble i;
-  ensemble f;
+    ensemble transition[TAILLE][TAILLE];
+    ensemble i;
+    ensemble f;
 } afn;
 
 // Renvoie 1 si l'état en paramètre est puit et 0 sinon
-int est_puit(afn a, int etat);
+int afn_estPuit(afn a, int etat);
+
+// Determinise l'AFN a
+int afn_determiniser(afn a, afn afd);
